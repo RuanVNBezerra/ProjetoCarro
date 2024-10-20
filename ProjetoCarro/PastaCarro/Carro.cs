f05ae7080cfab59768af96781ec699a56727422e
RuanVNBezerra <164013416+RuanVNBezerra@users.noreply.github.com>
2024-10-20 15:44:08 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoCarro/PastaCarro/Carro.cs
@@ -0,0 +1,36 @@
+﻿namespace ProjetoCarro.PastaCarro;
+
+public class Carro
+{ // atributos simples de um carro. 
+  // encapsulando esses atributos para que sejá usado só na classe Carro.
+    private string marca;
+    private int ano;
+    private string modelo;
+    private int atualVelocidade;
+
+    public Carro(string marca, int ano, string modelo)
+    { // diferenciado para o compilador os atributos da Classe (private marca, etc...) dos parâmetros usados usando this.
+        this.marca = marca;
+        this.ano = ano;
+        this.modelo = modelo;
+        this.atualVelocidade = 0;
+    }
+    public void Acelerar(int incremento)
+    { // função para aumentar a velocidade do carro.
+        atualVelocidade += incremento;
+    }
+    public void Desacelerar(int decremento)
+    { // função para desacelerar o carro.
+        atualVelocidade -= decremento;
+
+      // se a velocidade for zero quando usado o desacelerar, o carro recebe 0 de velocidade.
+      if(atualVelocidade < 0)
+        {
+            atualVelocidade = 0;
+        }
+    }
+    public void ExibirInformacoes()
+    { // aqui será exibido todas as informaçôes do carro.
+        Console.WriteLine($"Marca do Carro: {marca}, Ano do Carro: {ano}, Modelo do Carro: {modelo}, Velocidade Atual do Carro: {atualVelocidade} KM/h . ");
+    }
+}
---
+++ ProjetoCarro/Program.cs
@@ -0,0 +1,28 @@
+﻿//chamando a "PastaCarro" para o arquivo principal.
+using ProjetoCarro.PastaCarro;
+using System.Reflection;
+using System.Text.RegularExpressions;
+// nome do espaço principal.
+namespace ProjetoCarro;
+// projeto simples sobre um carro.
+internal class Program
+{
+    static void Main()
+    {
+        // criando um objeto Carro
+        Carro meuCarro = new Carro("suzuki", 2004, "Suzuki Swift");
+        //acelerando o Carro
+        meuCarro.Acelerar(40);
+        //Exibindo todas as informações acima.
+        meuCarro.ExibirInformacoes();
+        //desacelerando o Carro.
+        meuCarro.Desacelerar(39);
+        //Exibindo denovo as informações.
+        meuCarro.ExibirInformacoes();
+
+        //SAIDA DEPOIS DE ACELERAR: Marca suzuki, Ano do Carro 2004, Modelo do Carro Suzuki Swift, Velocidade Atual do Carro: 40 KM/h .
+        //SAIDA DEPOIS DE DESACELERAR: Marca suzuki, Ano do Carro 2004, Modelo do Carro Suzuki Swift, Velocidade Atual do Carro: 1 KM / h.
+
+        //fim do projeto usando classes e modificadores de acesso.
+    }
+}
